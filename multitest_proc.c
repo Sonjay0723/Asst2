4677208b3251a63d57fa2dcf69d750abb2bf44a3
Jayson R Pitta <39282131+Sonjay0723@users.noreply.github.com>
2019-11-21 18:36:49 -0500

    Update multitest_proc.c

--- multitest_proc.c
+++ multitest_proc.c
@@ -8,6 +8,8 @@
 int search(int* array, int target, int length) {
   int processes = 0;
   int size = 0;
+  int res = -1;
+  int PorC = 0;
   if (length > 250) {
     size = length;
     while (size > 250) {
@@ -26,11 +28,11 @@ int search(int* array, int target, int length) {
     children++;
   }
 
-  int i, j;
+  int i, j, k;
   for (i = (children * size) + leftover; i < length; i++) {
     if (array[i] == target) {
-      printf("Found in the parent process\n");
-      return i;
+      PorC = 1;
+      res = i;
     }
   }
 
@@ -42,24 +44,62 @@ int search(int* array, int target, int length) {
       int index = childNum % children;
 
       if (leftover > 0 && i == children - 1) {
+        k = 0;
         for (j = (index * size); j < ((index * size) + leftover); j++) {
           if (array[j] == target) {
-            printf("Found in the child process\n");
-            return j;
+            if (PorC == 0) {
+              PorC = 2;
+            }
+            printf("J: %d\n", j);
+            printf("K: %d\n", k);
+            res = j;
+            exit(j);
           }
         }
       }
       else {
+        k = 0;
         for (j = (index * size); j < ((index + 1) * size); j++) {
           if (array[j] == target) {
-            printf("Found in the child process\n");
-            return j;
+            if (PorC == 0) {
+              PorC = 2;
+            }
+            printf("J: %d\n", j);
+            printf("K: %d\n", k);
+            res = j;
+            exit(j);
           }
         }
+      }
+
+      exit(length + 1);
+    }
+    else if(child < 0) {
+      printf("Error: child invalid\n");
+      exit(-1);
+    }
+    else {
+      int status = 0;
+      waitpid(child, &status, 0);
+      int exitNum = WEXITSTATUS(status);
+      if (exitNum < (length + 1)) {
+        if (PorC == 0) {
+          PorC = 2;
+        }
+        printf("exitNum: %d\n", exitNum);
+        res = exitNum;
       }
     }
   }
 
-  printf("Target not found\n");
-  return -1;
+  if (res == -1) {
+    printf("Target not found\n");
+  }
+  else if (PorC == 1) {
+    printf("Target found in parent process\n");
+  }
+  else if (PorC == 2) {
+    printf("Target found in child process\n");
+  }
+  return res;
 }
