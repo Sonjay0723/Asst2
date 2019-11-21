5db874c508fe0ada068e303744a732be344d1f79
eshanw48 <50282448+eshanw48@users.noreply.github.com>
2019-11-20 21:57:22 -0500

    Update multitest.h

--- multitest.h
+++ multitest.h
@@ -4,27 +4,13 @@
 #include <stdlib.h>
 
 /* Redefining the search function, using macros. */
-#define searchArr(size) search(size,__FILE__,__LINE__)
+#define searchArr(array, target, size) search(array, target, size)
 
-// CHANGE BELOW
-/* Linked List which contains the metadata of each block. */
-typedef struct Node{
-  short int size; // Size of the block.
-  short int isFree; // 1=free, 0=in use
-  struct Node* next; // Points to the next metadata block.
-}Node;
-
-static char myblock[4096]; // This array is considered as our memory. It acts as our heap.
-
-static Node* freePtr=(void*) myblock; // Points to the memory.
+/* Determine if process or thread is running. */
+extern const char *name;
 
 /* Function Signatures. */
-void setUp();
-void divide(Node* bigBlock, size_t size);
-void* mymalloc(size_t size, char* file, int line);
-void combine();
-void myfree(void* ptr, char* file, int line);
-int inMem(struct Node *n);
+int search(int* array, int target, int size);
 
 #endif
 
