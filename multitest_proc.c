da8dd091deddada268c4eb9dfe9d5784fcadb932
Jayson R Pitta <39282131+Sonjay0723@users.noreply.github.com>
2019-11-22 00:11:27 -0500

    Update multitest_proc.c

--- multitest_proc.c
+++ multitest_proc.c
@@ -6,88 +6,65 @@
 #include <sys/types.h>
 
 int search(int* array, int target, int length) {
-  int processes = 0;
-  int size = 0;
   int res = -1;
+  int processes = 0;
+  int size;
+  int i;
   int PorC = 0;
+
   if (length > 250) {
     size = length;
     while (size > 250) {
-      size /= 2;
       processes++;
+      size /= 2;
     }
   }
   else {
-    processes = 2;
+    processes = 1;
     size = length / 2;
   }
 
   int children = pow(2, processes) - 1;
-  int leftover = length - ((children + 1) * size);
-  if (leftover > 0) {
-    children++;
-  }
 
-  int i, j, k;
-  for (i = (children * size) + leftover; i < length; i++) {
+  for (i = (children * size); i < length; i++) {
     if (array[i] == target) {
-      PorC = 1;
       res = i;
+      PorC = 1;
     }
   }
 
   for (i = 0; i < children; i++) {
     int child = fork();
 
-    if (child == 0) {
-      int childNum = getpid();
-      int index = childNum % children;
-
-      if (leftover > 0 && i == children - 1) {
-        k = 0;
-        for (j = (index * size); j < ((index * size) + leftover); j++) {
-          if (array[j] == target) {
-            if (PorC == 0) {
-              PorC = 2;
-            }
-            printf("J: %d\n", j);
-            printf("K: %d\n", k);
-            res = j;
-            exit(j);
-          }
-        }
-      }
-      else {
-        k = 0;
-        for (j = (index * size); j < ((index + 1) * size); j++) {
-          if (array[j] == target) {
-            if (PorC == 0) {
-              PorC = 2;
-            }
-            printf("J: %d\n", j);
-            printf("K: %d\n", k);
-            res = j;
-            exit(j);
-          }
-        }
-      }
-
-      exit(length + 1);
-    }
-    else if(child < 0) {
-      printf("Error: child invalid\n");
+    if (child < 0) {
+      printf("Error: Invalid Child\n");
       exit(-1);
     }
-    else {
-      int status = 0;
-      waitpid(child, &status, 0);
-      int exitNum = WEXITSTATUS(status);
-      if (exitNum < (length + 1)) {
-        if (PorC == 0) {
+    else if (child == 0) {
+      int childNum = getpid();
+      int j;
+      int k = 0;
+
+      int index = childNum % children;
+
+      for (j = (index * size); j < ((index + 1) * size); j++) {
+        k++;
+        if (array[j] == target) {
           PorC = 2;
+          res = j;
+          exit(k);
         }
-        printf("exitNum: %d\n", exitNum);
-        res = exitNum;
+      }
+
+      exit(251);
+    }
+    else {
+      int stat = 0;
+      waitpid(child, &stat, 0);
+      int exitNum = WEXITSTATUS(stat);
+      if (exitNum < 251) {
+        int index = child % children;
+        res = (index * size) - 1 + exitNum;
       }
     }
   }
@@ -98,8 +75,9 @@ int search(int* array, int target, int length) {
   else if (PorC == 1) {
     printf("Target found in parent process\n");
   }
-  else if (PorC == 2) {
+  else {
     printf("Target found in child process\n");
   }
+
   return res;
 }
